dbe45970a6966ee89b114474781eec10c16de572
Azhes <graystonea@gmail.com>
2021-04-07 15:51:40 -0700

    add function to make noteArea visible and createNote hidden when createNote is clicked

--- JS/app.js
+++ JS/app.js
@@ -7,7 +7,17 @@ const checkbox = document.getElementById('checkbox')
 checkbox.addEventListener('change', () => {
     // change theme
     var slideout = document.getElementById("slideout")
+    var createNote = document.getElementById("createNote")
 
     document.body.classList.toggle('light')
     slideout.classList.toggle('light')
+    createNote.classList.toggle('light')
+})
+
+createNote.addEventListener("click", () => {
+    //makes the textarea visible
+    var noteArea = document.getElementById("noteArea")
+    var createNote = document.getElementById("createNote")
+    noteArea.classList.toggle('visible')
+    createNote.classList.toggle('hidden')
 })
