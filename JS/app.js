d96889e487a67bb09761a913bc4aeda587b5b725
Azhes <graystonea@gmail.com>
2021-04-07 14:56:27 -0700

    create container for note title and note body as well as save note and delete note buttons

--- JS/app.js
+++ JS/app.js
@@ -6,14 +6,6 @@ const checkbox = document.getElementById('checkbox')
 
 checkbox.addEventListener('change', () => {
     // change theme
-    var slideout = document.getElementById("slideout")
-    var newnote = document.getElementById("newnote")
-    var nav = document.getElementById("nav")
-    var nav2 = document.getElementById("nav2")
 
     document.body.classList.toggle('light');
-    slideout.classList.toggle('light')
-    newnote.classList.toggle('light')
-    nav.classList.toggle('light')
-    nav2.classList.toggle('light')
 })
