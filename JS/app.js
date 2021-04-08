98725b5f0fd73bd0ee5c3ba3beea5b2bc4d4cfd3
Azhes <graystonea@gmail.com>
2021-04-07 20:41:11 -0700

    style noteArea with flexbox and add functionality to darkmodetoggle

--- JS/app.js
+++ JS/app.js
@@ -8,10 +8,18 @@ checkbox.addEventListener('change', () => {
     // change theme
     var slideout = document.getElementById("slideout")
     var createNote = document.getElementById("createNote")
+    var saveButton = document.getElementById("saveButton")
+    var deleteButton = document.getElementById("deleteButton")
+    var noteTitle = document.getElementById("noteTitle")
+    var noteBody = document.getElementById("noteBody")
 
     document.body.classList.toggle('light')
     slideout.classList.toggle('light')
     createNote.classList.toggle('light')
+    saveButton.classList.toggle('light')
+    deleteButton.classList.toggle('light')
+    noteTitle.classList.toggle('light')
+    noteBody.classList.toggle('light')
 })
 
 createNote.addEventListener("click", () => {
